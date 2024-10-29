183f9152cdc793b5ac6d119792e81ab972457bd5
arthur <arthurazevedods@gmail.com>
2024-10-29 14:19:29 -0300

    localhost

--- src/Pages/LoginHome.jsx
+++ src/Pages/LoginHome.jsx
@@ -1,10 +1,15 @@
 import Navbar from "../components/Navbar";
-import { Link,  } from 'react-router-dom';
+import { Link, } from 'react-router-dom';
+
+
 const LodinHome = () => {
+
+
     return (
         <>
             <Navbar />
             <div className="container">
+
                 <Link to='/login' className="btn button-link">Login</Link>
             </div>
         </>
