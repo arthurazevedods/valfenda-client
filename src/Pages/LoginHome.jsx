fbe31f3f5d0095a60f933fbd9aea0da25c786962
arthur <arthurazevedods@gmail.com>
2024-10-29 14:29:35 -0300

    autores em login home

--- src/Pages/LoginHome.jsx
+++ src/Pages/LoginHome.jsx
@@ -1,9 +1,31 @@
 import Navbar from "../components/Navbar";
 import { Link, } from 'react-router-dom';
-
-
+import { useEffect, useState } from "react";
+import axios from 'axios';
+const API_URL = "https://valfenda-api.onrender.com"
 const LodinHome = () => {
+    const [authors, setAuthors] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
 
+    useEffect(() => {
+        // Função para buscar os autores do backend
+        const fetchAuthors = async () => {
+            try {
+                const response = await axios.get(`${API_URL}/authors`);
+                setAuthors(response.data); // Armazenando os autores no estado
+                setLoading(false); // Parando o loading
+            } catch (e) {
+                setError('Erro ao carregar autores:', e);
+                setLoading(false); // Parando o loading em caso de erro
+            }
+        };
+
+        fetchAuthors(); // Chamando a função ao montar o componente
+    }, []); // O array vazio faz com que o efeito rode uma vez ao montar o componente
+
+    if (loading) return <p>Carregando autores...</p>;
+    if (error) return <p>{error}</p>;
 
     return (
         <>
@@ -12,6 +34,14 @@ const LodinHome = () => {
 
                 <Link to='/login' className="btn button-link">Login</Link>
             </div>
+            <div className="container">
+            <h3>Autores:</h3>
+            <ul>
+                {authors.map((author) => (
+                    <li key={author.id}>{author.name}</li> // Renderizando a lista de autores
+                ))}
+            </ul>
+        </div>
         </>
     );
 }
